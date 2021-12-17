217bf3d0b43822309423c2dcd11a3f3931e60b41
MukhametovUR <ural-lan4x4@yandex.ru>
2021-12-17 12:59:53 +0300

    homework2

---
+++ hw2_script4.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const sum = (a,b) =>{
+    return a + b;
+};
+const raznost = (a,b) =>{
+    return a - b;
+};
+const proizv = (a,b) =>{
+    return a * b;
+};
+const delenie = (a,b) =>{
+    return a / b;
+};
+
+console.log(sum(6,2));
+console.log(raznost(6,2));
+console.log(proizv(6,2));
+console.log(delenie(6,2));
+
